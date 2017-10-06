90456837b66285d288b2a197f90267e01e4b6830
fogo <tochaman@gmail.com>
2017-10-06 09:56:01 -0300

    fixup! fixup! fixup! fixup! wip: changing to support worker pool

--- include/spdlog/details/async_log_helper.h
+++ include/spdlog/details/async_log_helper.h
@@ -248,12 +248,7 @@ public:
     void set_error_handler(const std::string& logger_name, spdlog::log_err_handler err_handler) override;
 
 private:
-    pooled_log_helper(size_t queue_size,
-                      const log_err_handler err_handler,
-                      const async_overflow_policy overflow_policy = async_overflow_policy::block_retry,
-                      const std::function<void()>& worker_warmup_cb = nullptr,
-                      const std::chrono::milliseconds& flush_interval_ms = std::chrono::milliseconds::zero(),
-                      const std::function<void()>& worker_teardown_cb = nullptr);
+    static std::shared_ptr<pooled_log_helper> _instance;
 
     // worker thread
     std::vector<std::thread> _worker_threads;
@@ -271,6 +266,13 @@ private:
     std::mutex _flush_mutex;
 
     std::atomic<bool> _terminate_requested;
+
+    pooled_log_helper(size_t queue_size,
+                      const log_err_handler err_handler,
+                      const async_overflow_policy overflow_policy = async_overflow_policy::block_retry,
+                      const std::function<void()>& worker_warmup_cb = nullptr,
+                      const std::chrono::milliseconds& flush_interval_ms = std::chrono::milliseconds::zero(),
+                      const std::function<void()>& worker_teardown_cb = nullptr);
 
     // TODO: may be moved back to base
     // worker thread warmup callback - one can set thread priority, affinity, etc
@@ -340,6 +342,21 @@ inline spdlog::details::async_log_helper::~async_log_helper()
     }
 }
 
+inline spdlog::details::pooled_log_helper::~pooled_log_helper()
+{
+    try
+    {
+        push_msg(async_msg("", async_msg_type::terminate));
+        for (auto& worker_thread: _worker_threads)
+        {
+            worker_thread.join();
+        }
+    }
+    catch (...) // don't crash in destructor
+    {
+    }
+}
+
 
 //Try to push and block until succeeded (if the policy is not to discard when the queue is full)
 inline void spdlog::details::base_async_log_helper::log(const details::log_msg& msg)
@@ -371,6 +388,14 @@ inline void spdlog::details::async_log_helper::flush(const std::string& logger_n
         wait_empty_q(); //return only make after the above flush message was processed
 }
 
+inline void spdlog::details::pooled_log_helper::flush(const std::string& logger_name, bool wait_for_q)
+{
+    // TODO: may be moved back to base
+    push_msg(async_msg(logger_name, async_msg_type::flush));
+    if (wait_for_q)
+        wait_empty_q(); //return only make after the above flush message was processed
+}
+
 inline void spdlog::details::async_log_helper::worker_loop()
 {
     if (_worker_warmup_cb) _worker_warmup_cb();
@@ -390,6 +415,36 @@ inline void spdlog::details::async_log_helper::worker_loop()
         catch (...)
         {
             _err_handler("Unknown exception");
+        }
+    }
+    if (_worker_teardown_cb) _worker_teardown_cb();
+
+
+}
+
+inline void spdlog::details::pooled_log_helper::worker_loop()
+{
+    if (_worker_warmup_cb) _worker_warmup_cb();
+    auto last_pop = details::os::now();
+    auto last_flush = last_pop;
+    auto active = true;
+    while (active)
+    {
+        try
+        {
+            active = process_next_msg(last_pop, last_flush);
+        }
+        catch (const std::exception &ex)
+        {
+            for (auto& logger: _loggers) {
+                logger.second.err_handler(ex.what());
+            }
+        }
+        catch (...)
+        {
+            for (auto& logger: _loggers) {
+                logger.second.err_handler("Unknown exception");
+            }
         }
     }
     if (_worker_teardown_cb) _worker_teardown_cb();
@@ -443,6 +498,54 @@ inline bool spdlog::details::async_log_helper::process_next_msg(log_clock::time_
     }
 }
 
+inline bool spdlog::details::pooled_log_helper::process_next_msg(log_clock::time_point& last_pop, log_clock::time_point& last_flush)
+{
+    async_msg incoming_async_msg;
+
+    if (_q.dequeue(incoming_async_msg))
+    {
+        last_pop = details::os::now();
+        switch (incoming_async_msg.msg_type)
+        {
+            case async_msg_type::flush:
+            {
+                std::lock_guard<std::mutex> _lock(_flush_mutex);
+                _flush_requested.push_back(incoming_async_msg.logger_name);
+                break;
+            }
+
+            case async_msg_type::terminate:
+                _terminate_requested = true;
+                break;
+
+            default:
+                log_msg incoming_log_msg;
+                incoming_async_msg.fill_log_msg(incoming_log_msg);
+
+                auto& logger = _loggers[incoming_async_msg.logger_name];
+                logger.formatter->format(incoming_log_msg);
+                for (auto &s : logger.sinks)
+                {
+                    if (s->should_log(incoming_log_msg.level))
+                    {
+                        s->log(incoming_log_msg);
+                    }
+                }
+        }
+        return true;
+    }
+
+        // Handle empty queue..
+        // This is the only place where the queue can terminate or flush to avoid losing messages already in the queue
+    else
+    {
+        auto now = details::os::now();
+        handle_flush_interval(now, last_flush);
+        sleep_or_yield(now, last_pop);
+        return !_terminate_requested;
+    }
+}
+
 // flush all sinks if _flush_interval_ms has expired
 inline void spdlog::details::async_log_helper::handle_flush_interval(log_clock::time_point& now, log_clock::time_point& last_flush)
 {
@@ -456,11 +559,40 @@ inline void spdlog::details::async_log_helper::handle_flush_interval(log_clock::
     }
 }
 
+inline void spdlog::details::pooled_log_helper::handle_flush_interval(log_clock::time_point& now, log_clock::time_point& last_flush)
+{
+    if (_flush_mutex.try_lock()) {
+        if (!_flush_requested.empty())
+        {
+            for (auto &name: _flush_requested)
+                for (auto &s : _loggers[name].sinks)
+                    s->flush();
+            now = last_flush = details::os::now();
+            _flush_requested.clear();
+        } else if ((_flush_interval_ms != std::chrono::milliseconds::zero() &&
+                    now - last_flush >= _flush_interval_ms) ||_terminate_requested)
+        {
+            for (auto &logger: _loggers)
+                for (auto &s : logger.second.sinks)
+                    s->flush();
+            now = last_flush = details::os::now();
+            _flush_requested.clear();
+        }
+
+        // TODO: how lock_guard works w/ try_lock strategy?
+        _flush_mutex.unlock();
+    }
+}
+
 inline void spdlog::details::async_log_helper::set_formatter(const std::string& logger_name, formatter_ptr msg_formatter)
 {
     _formatter = msg_formatter;
 }
 
+inline void spdlog::details::pooled_log_helper::set_formatter(const std::string& logger_name, formatter_ptr msg_formatter)
+{
+    _loggers[logger_name].formatter = msg_formatter;
+}
 
 // spin, yield or sleep. use the time passed since last message as a hint
 inline void spdlog::details::base_async_log_helper::sleep_or_yield(const spdlog::log_clock::time_point& now, const spdlog::log_clock::time_point& last_op_time)
@@ -502,5 +634,7 @@ inline void spdlog::details::async_log_helper::set_error_handler(const std::stri
     _err_handler = err_handler;
 }
 
-
-
+inline void spdlog::details::pooled_log_helper::set_error_handler(const std::string& logger_name, spdlog::log_err_handler err_handler)
+{
+    _loggers[logger_name].err_handler = err_handler;
+}
